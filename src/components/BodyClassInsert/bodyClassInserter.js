5e4617cc1be8605f3d54219f9ebc85b1de10e27c
VictorBlasques <159483483+VictorBlasques@users.noreply.github.com>
2024-10-21 10:32:56 -0300

    fix: new.css

--- src/components/BodyClassInsert/bodyClassInserter.js
+++ src/components/BodyClassInsert/bodyClassInserter.js
@@ -1,4 +1,4 @@
 // Função para adicionar uma classe ao body
 export function addBodyClass(className) {
-    document.body.classList.add(className);
-  }
+  document.body.classList.add(className);
+}
