2762942a45fc35985fbabbed0b0e3f75aa91f7e6
viniciusamorim <vinicius.amorim@segurpro.com.br>
2024-11-04 08:26:29 -0300

    fix:table-dashboard

--- src/pages/Dashboard/tableDashboard.js
+++ src/pages/Dashboard/tableDashboard.js
@@ -18,7 +18,7 @@ import LastPageIcon from "@mui/icons-material/LastPage";
 import ModalLog from "../../components/Modal_Logs";
 import { Link } from "react-router-dom";
 import { Close, Search } from "@mui/icons-material";
-import { TableHead } from "@mui/material";
+import { TableHead, TableSortLabel } from "@mui/material";
 
 // Função para salvar a página no localStorage
 const savePageToLocalStorage = (page) => {
@@ -106,6 +106,8 @@ export default function CustomPaginationActionsTable(props) {
   // Carregar a página inicial do localStorage
   const [page, setPage] = React.useState(loadPageFromLocalStorage());
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("apr_id");
 
   // Evitar saltos na última página quando houver registros vazios.
   const emptyRows =
@@ -122,92 +124,204 @@ export default function CustomPaginationActionsTable(props) {
     savePageToLocalStorage(0); // Reseta para a primeira página e salva no localStorage
   };
 
+  const handleRequestSort = (property) => {
+    const isAscending = orderBy === property && order === "asc";
+    setOrder(isAscending ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const sortedRows = React.useMemo(() => {
+    return chamados.sort((a, b) => {
+      if (orderBy in a && orderBy in b) {
+        if (order === "asc") {
+          return a[orderBy] < b[orderBy] ? -1 : 1;
+        } else {
+          return a[orderBy] > b[orderBy] ? -1 : 1;
+        }
+      }
+      return 0;
+    });
+  }, [chamados, order, orderBy]);
+
   return (
-    <TableHead>
-      <TableRow>
-        <TableCell align="center">
-          <TableSortLabel
-            active={orderBy === "apr_id"}
-            direction={orderBy === "apr_id" ? order : "asc"}
-            onClick={() => handleRequestSort("apr_id")}
-          >
-            APR ID
-          </TableSortLabel>
-        </TableCell>
-        <TableCell align="center">
-          <TableSortLabel
-            active={orderBy === "motivo_apr"}
-            direction={orderBy === "motivo_apr" ? order : "asc"}
-            onClick={() => handleRequestSort("motivo_apr")}
-          >
-            MOTIVO
-          </TableSortLabel>
-        </TableCell>
-        <TableCell align="center">
-          <TableSortLabel
-            active={orderBy === "site_id.Sigla"}
-            direction={orderBy === "site_id.Sigla" ? order : "asc"}
-            onClick={() => handleRequestSort("site_id.Sigla")}
-          >
-            Sigla-UF
-          </TableSortLabel>
-        </TableCell>
-        <TableCell align="center">
-          <TableSortLabel
-            active={orderBy === "site_id.Nome"}
-            direction={orderBy === "site_id.Nome" ? order : "asc"}
-            onClick={() => handleRequestSort("site_id.Nome")}
-          >
-            Nome
-          </TableSortLabel>
-        </TableCell>
-        <TableCell align="center">
-          <TableSortLabel
-            active={orderBy === "site_id.tipoSite"}
-            direction={orderBy === "site_id.tipoSite" ? order : "asc"}
-            onClick={() => handleRequestSort("site_id.tipoSite")}
-          >
-            Tipo Site
-          </TableSortLabel>
-        </TableCell>
-        <TableCell align="center">
-          <TableSortLabel
-            active={orderBy === "site_id.Cidade"}
-            direction={orderBy === "site_id.Cidade" ? order : "asc"}
-            onClick={() => handleRequestSort("site_id.Cidade")}
-          >
-            Municipio
-          </TableSortLabel>
-        </TableCell>
-        <TableCell align="center">
-          <TableSortLabel
-            active={orderBy === "status"}
-            direction={orderBy === "status" ? order : "asc"}
-            onClick={() => handleRequestSort("status")}
-          >
-            Status
-          </TableSortLabel>
-        </TableCell>
-        <TableCell align="center">
-          <TableSortLabel
-            active={orderBy === "created"}
-            direction={orderBy === "created" ? order : "asc"}
-            onClick={() => handleRequestSort("created")}
-          >
-            Data
-          </TableSortLabel>
-        </TableCell>
-        <TableCell align="center">
-          <TableSortLabel
-            active={orderBy === "porcentagem_resp_area"}
-            direction={orderBy === "porcentagem_resp_area" ? order : "asc"}
-            onClick={() => handleRequestSort("porcentagem_resp_area")}
-          >
-            %
-          </TableSortLabel>
-        </TableCell>
-        <TableCell align="center"></TableCell>
-      </TableRow>
-    </TableHead>
+    <TableContainer component={Paper}>
+      <Table
+        className="table-dashbaord"
+        size="small"
+        aria-label="a dense table"
+      >
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">
+              <TableSortLabel
+                active={orderBy === "apr_id"}
+                direction={orderBy === "apr_id" ? order : "asc"}
+                onClick={() => handleRequestSort("apr_id")}
+              >
+                APR ID
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="center">
+              <TableSortLabel
+                active={orderBy === "motivo_apr"}
+                direction={orderBy === "motivo_apr" ? order : "asc"}
+                onClick={() => handleRequestSort("motivo_apr")}
+              >
+                MOTIVO
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="center">
+              <TableSortLabel
+                active={orderBy === "site_id.Sigla"}
+                direction={orderBy === "site_id.Sigla" ? order : "asc"}
+                onClick={() => handleRequestSort("site_id.Sigla")}
+              >
+                Sigla-UF
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="center">
+              <TableSortLabel
+                active={orderBy === "site_id.Nome"}
+                direction={orderBy === "site_id.Nome" ? order : "asc"}
+                onClick={() => handleRequestSort("site_id.Nome")}
+              >
+                Nome
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="center">
+              <TableSortLabel
+                active={orderBy === "site_id.tipoSite"}
+                direction={orderBy === "site_id.tipoSite" ? order : "asc"}
+                onClick={() => handleRequestSort("site_id.tipoSite")}
+              >
+                Tipo Site
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="center">
+              <TableSortLabel
+                active={orderBy === "site_id.Cidade"}
+                direction={orderBy === "site_id.Cidade" ? order : "asc"}
+                onClick={() => handleRequestSort("site_id.Cidade")}
+              >
+                Municipio
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="center">
+              <TableSortLabel
+                active={orderBy === "status"}
+                direction={orderBy === "status" ? order : "asc"}
+                onClick={() => handleRequestSort("status")}
+              >
+                Status
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="center">
+              <TableSortLabel
+                active={orderBy === "created"}
+                direction={orderBy === "created" ? order : "asc"}
+                onClick={() => handleRequestSort("created")}
+              >
+                Data
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="center">
+              <TableSortLabel
+                active={orderBy === "porcentagem_resp_area"}
+                direction={orderBy === "porcentagem_resp_area" ? order : "asc"}
+                onClick={() => handleRequestSort("porcentagem_resp_area")}
+              >
+                %
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="center"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? sortedRows.slice(
+              page * rowsPerPage,
+              page * rowsPerPage + rowsPerPage
+            )
+            : sortedRows
+          ).map((row, index) => (
+            <TableRow key={row.id}>
+              <TableCell data-label="ID" align="center">
+                {row.apr_id}
+              </TableCell>
+              <TableCell data-label="Motivo APR" align="center">
+                {row.motivo_apr}
+              </TableCell>
+              <TableCell data-label="Sigla-UF" align="center">
+                {row.site_id.Sigla} - {row.site_id.Estado}
+              </TableCell>
+              <TableCell data-label="Nome" align="left">
+                {row.site_id.Nome}
+              </TableCell>
+              <TableCell data-label="Tipo Site" align="center">
+                {row.site_id.tipoSite}
+              </TableCell>
+              <TableCell data-label="Municipio" align="center">
+                {row.site_id.Cidade}
+              </TableCell>
+              <TableCell data-label="Status" align="center">
+                {row.status}
+              </TableCell>
+              <TableCell data-label="Data" align="center">
+                {row.created}
+              </TableCell>
+              <TableCell data-label="%" align="center">
+                {row.porcentagem_resp_area}
+              </TableCell>
+              <TableCell style={{ width: 160 }} align="center">
+                <Link to={`/open/${row.id}`}>
+                  <IconButton color="info">
+                    <Search />
+                  </IconButton>
+                </Link>
+                {(user.nivel === "administrador" ||
+                  user.nivel === "revisor") && (
+                    <IconButton
+                      onClick={() => updateStatus(row.id, index)}
+                      color="error"
+                      aria-label="add an alarm"
+                    >
+                      <Close />
+                    </IconButton>
+                  )}
+                {(user.nivel === "administrador" ||
+                  user.nivel === "revisor") && <ModalLog chamadoId={row.id} />}
+              </TableCell>
+            </TableRow>
+          ))}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={10} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 50, { label: "Todos", value: -1 }]}
+              colSpan={10}
+              count={chamados.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              slotProps={{
+                select: {
+                  inputProps: {
+                    "aria-label": "chamados per page",
+                  },
+                  native: true,
+                },
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
   );
 }
