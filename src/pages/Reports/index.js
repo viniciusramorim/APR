32a56ec5272689dfa095d073e95dd9268e7fe3e4
VictorBlasques <victor.blasques@telefonica.com>
2025-05-20 15:42:06 -0300

    Update index.js

--- src/pages/Reports/index.js
+++ src/pages/Reports/index.js
@@ -165,11 +165,179 @@ export default function Reports() {
     }
   }
 
+  // async function updateState(snapshot) {
+  //   setLoading(true)
+  //   const relatorioApr = [];
+  //   const promises = snapshot.map(doc => {
+  //     // Verifica se o documento tem a propriedade
+  //     if (doc.created !== undefined) {
+  //       return v0(doc).then(result => {
+  //         if (includeQuestions) {
+  //           if (doc.checklist && doc.status !== "Com Exceção") {
+  //             doc.checklist.forEach((blocoQuestion) => {
+  //               blocoQuestion[1].forEach((question) => {
+  //                 if ((question.resp === "" && question.optionListResp !== "") || (question.resp !== "")) {
+  //                   console.log(question.areaResposavel, doc.id)
+  //                   relatorioApr.push({
+  //                     ID: doc.id,
+  //                     DATA: format(doc.created.toDate(), "dd/MM/yyyy HH:mm:ss"),
+  //                     STATUS: doc.status,
+  //                     MOTIVO: doc.motivo_apr,
+  //                     CLASSIFICACAO: calculatePontos(doc.peso),
+  //                     PESO: doc.peso,
+  //                     QUESTIONS: question.question,
+  //                     QUESTIONS_RESP: question.resp,
+  //                     QUESTIONS_CHECKS: question.optionListResp.toString(),
+  //                     QUESTIONS_AREA_RESPONSAVEL: question.areaResposavel ? question.areaResposavel.toString() : "",
+  //                     QUESTIONS_RESPTEXTAREA: question.respTextArea,
+  //                     QUESTIONS_RESPGABARITO: question.respGabarito,
+  //                     QUESTIONS_PA: question.openPA,
+  //                     QUESTION_PA_DATA: question.resp_pa_data
+  //                       ? format(
+  //                         question.resp_pa_data.toDate(),
+  //                         "dd/MM/yyyy HH:mm:ss"
+  //                       )
+  //                       : "",
+  //                     QUESTION_PA_RESP: question.resp_pa_selectedOption || "",
+  //                     QUESTION_PA_NOME: question.resp_pa_user_name || "",
+  //                     SIGLA: doc.site_id.Sigla,
+  //                     SIGLA_GVT: doc.site_id.Sigla_GVT,
+  //                     TIPO_CHECKLIST: doc.site_id.tipoSite,
+  //                     NOME_SITE: doc.site_id.Nome,
+  //                     LAT_SITE: doc.site_id.Latitude,
+  //                     LNG_SITE: doc.site_id.Longitude,
+  //                     UF_SITE: doc.site_id.Estado,
+  //                     END_SITE: doc.site_id.Endereco,
+  //                     MUNICIPIO_SITE: doc.site_id.Cidade,
+  //                     BAIRRO_SITE: doc.site_id.Bairro,
+  //                     CEP_SITE: doc.site_id.CEP,
+  //                     CRITICIDADE_SITE: doc.site_id.critical,
+  //                     ABERTURA_LAT: doc.locationCreated ? doc.locationCreated.latitude : '-',
+  //                     ABERTURA_LNG: doc.locationCreated ? doc.locationCreated.logitude : '-',
+  //                     ABERTURA_PERIMETRO: doc.locationCreated ? doc.locationCreated.perimetro : '-',
+  //                     TEMP_INCIO: doc.tempoConclusao ? format(
+  //                       doc.tempoConclusao.inicio.toDate(),
+  //                       "dd/MM/yyyy HH:mm:ss"
+  //                     ) : "-",
+  //                     TEMP_TERMINO: doc.tempoConclusao ? format(
+  //                       doc.tempoConclusao.conclusao.toDate(),
+  //                       "dd/MM/yyyy HH:mm:ss"
+  //                     ) : '-',
+  //                     TEMP_EFETUADO: doc.tempoConclusao ? Math.ceil(
+  //                       (doc.tempoConclusao.conclusao.toDate() -
+  //                         doc.tempoConclusao.inicio.toDate()) /
+  //                       (1000 * 60)
+  //                     ) : '-',
+  //                     USER_ID: doc.user_id.uid,
+  //                     USER_NOME: doc.user_id.nome,
+  //                     USER_UF: doc.user_id.uf,
+  //                     V0: result,
+  //                   });
+  //                 }
+  //               });
+  //             });
+  //           } else if (doc.status === "Com Exceção") {
+  //             relatorioApr.push({
+  //               ID: doc.id,
+  //               DATA: format(doc.created.toDate(), "dd/MM/yyyy HH:mm:ss"),
+  //               STATUS: doc.status,
+  //               SIGLA: doc.site_id.Sigla,
+  //               SIGLA_GVT: doc.site_id.Sigla_GVT,
+  //               TIPO_CHECKLIST: doc.site_id.tipoSite,
+  //               NOME_SITE: doc.site_id.Nome,
+  //               LAT_SITE: doc.site_id.Latitude,
+  //               LNG_SITE: doc.site_id.Longitude,
+  //               UF_SITE: doc.site_id.Estado,
+  //               END_SITE: doc.site_id.Endereco,
+  //               MUNICIPIO_SITE: doc.site_id.Cidade,
+  //               BAIRRO_SITE: doc.site_id.Bairro,
+  //               CEP_SITE: doc.site_id.CEP,
+  //               CRITICIDADE_SITE: doc.site_id.critical,
+  //               ABERTURA_LAT: doc.locationCreated ? doc.locationCreated.latitude : '-',
+  //               ABERTURA_LNG: doc.locationCreated ? doc.locationCreated.logitude : '-',
+  //               ABERTURA_PERIMETRO: doc.locationCreated ? doc.locationCreated.perimetro : '-',
+  //               TEMP_INCIO: doc.tempoConclusao ? format(
+  //                 doc.tempoConclusao.inicio.toDate(),
+  //                 "dd/MM/yyyy HH:mm:ss"
+  //               ) : "-",
+  //               TEMP_TERMINO: doc.tempoConclusao ? format(
+  //                 doc.tempoConclusao.conclusao.toDate(),
+  //                 "dd/MM/yyyy HH:mm:ss"
+  //               ) : '-',
+  //               TEMP_EFETUADO: doc.tempoConclusao ? Math.ceil(
+  //                 (doc.tempoConclusao.conclusao.toDate() -
+  //                   doc.tempoConclusao.inicio.toDate()) /
+  //                 (1000 * 60)
+  //               ) : '-',
+  //               USER_ID: doc.user_id.uid,
+  //               USER_NOME: doc.user_id.nome,
+  //               USER_UF: doc.user_id.uf,
+  //               V0: "-",
+  //             });
+  //           }
+  //         } else {
+  //           relatorioApr.push({
+  //             ID: doc.id,
+  //             DATA: format(doc.created.toDate(), "dd/MM/yyyy HH:mm:ss"),
+  //             STATUS: doc.status,
+  //             MOTIVO: doc.motivo_apr,
+  //             CLASSIFICACAO: calculatePontos(doc.peso),
+  //             PESO: doc.peso,
+  //             SIGLA: doc.site_id.Sigla,
+  //             SIGLA_GVT: doc.site_id.Sigla_GVT,
+  //             TIPO_CHECKLIST: doc.site_id.tipoSite,
+  //             NOME_SITE: doc.site_id.Nome,
+  //             LAT_SITE: doc.site_id.Latitude,
+  //             LNG_SITE: doc.site_id.Longitude,
+  //             UF_SITE: doc.site_id.Estado,
+  //             END_SITE: doc.site_id.Endereco,
+  //             MUNICIPIO_SITE: doc.site_id.Cidade,
+  //             BAIRRO_SITE: doc.site_id.Bairro,
+  //             CEP_SITE: doc.site_id.CEP,
+  //             CRITICIDADE_SITE: doc.site_id.critical,
+  //             ABERTURA_LAT: doc.locationCreated ? doc.locationCreated.latitude : '-',
+  //             ABERTURA_LNG: doc.locationCreated ? doc.locationCreated.logitude : '-',
+  //             ABERTURA_PERIMETRO: doc.locationCreated ? doc.locationCreated.perimetro : '-',
+  //             TEMP_INCIO: doc.tempoConclusao ? format(
+  //               doc.tempoConclusao.inicio.toDate(),
+  //               "dd/MM/yyyy HH:mm:ss"
+  //             ) : "-",
+  //             TEMP_TERMINO: doc.tempoConclusao ? format(
+  //               doc.tempoConclusao.conclusao.toDate(),
+  //               "dd/MM/yyyy HH:mm:ss"
+  //             ) : '-',
+  //             TEMP_EFETUADO: doc.tempoConclusao ? Math.ceil(
+  //               (doc.tempoConclusao.conclusao.toDate() -
+  //                 doc.tempoConclusao.inicio.toDate()) /
+  //               (1000 * 60)
+  //             ) : '-',
+  //             USER_ID: doc.user_id.uid,
+  //             USER_NOME: doc.user_id.nome,
+  //             USER_UF: doc.user_id.uf,
+  //             V0: result,
+  //           });
+  //         }
+  //       });
+  //     } else {
+  //       return Promise.resolve(); // Retorna uma promise resolvida para documentos sem 'created'
+  //     }
+  //   });
+
+  //   // Aguarda a resolução de todas as Promises
+  //   Promise.all(promises).then(() => {
+  //     console.log(relatorioApr);
+  //     downloadExcel(relatorioApr);
+  //     setLoading(false)
+  //   }).catch(error => {
+  //     console.error("Erro ao processar documentos:", error);
+  //     setLoading(false)
+  //   });
+  // }
+
   async function updateState(snapshot) {
     setLoading(true)
     const relatorioApr = [];
     const promises = snapshot.map(doc => {
-      // Verifica se o documento tem a propriedade
       if (doc.created !== undefined) {
         return v0(doc).then(result => {
           if (includeQuestions) {
@@ -186,8 +354,8 @@ export default function Reports() {
                       PESO: doc.peso,
                       QUESTIONS: question.question,
                       QUESTIONS_RESP: question.resp,
-                      QUESTIONS_CHECKS: question.optionListResp.toString(),
-                      QUESTIONS_AREA_RESPONSAVEL: question.areaResposavel.toString(),
+                      QUESTIONS_CHECKS: question.optionListResp ? question.optionListResp.toString() : "",
+                      QUESTIONS_AREA_RESPONSAVEL: question.areaResposavel ? question.areaResposavel.toString() : "",
                       QUESTIONS_RESPTEXTAREA: question.respTextArea,
                       QUESTIONS_RESPGABARITO: question.respGabarito,
                       QUESTIONS_PA: question.openPA,
@@ -318,11 +486,10 @@ export default function Reports() {
           }
         });
       } else {
-        return Promise.resolve(); // Retorna uma promise resolvida para documentos sem 'created'
+        return Promise.resolve();
       }
     });
 
-    // Aguarda a resolução de todas as Promises
     Promise.all(promises).then(() => {
       console.log(relatorioApr);
       downloadExcel(relatorioApr);
